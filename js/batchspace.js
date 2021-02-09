a4e4442a0206942a76c49500ae7013c8b580ce33
Marcel <marcel@udiocollective.com>
2021-02-09 11:06:49 +0000

    first version to be online commit

--- js/batchspace.js
+++ js/batchspace.js
@@ -3,5 +3,6 @@ const nav_list = document.querySelector("nav ol");
 
 nav_toggle.addEventListener('click', event => {
     nav_list.classList.toggle("visible");
+    nav_toggle.classList.toggle("open");
     console.log('clicked');
 });
